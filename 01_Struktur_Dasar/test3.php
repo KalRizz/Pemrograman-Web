f8017e5b752435e1a2d326a256b4d7169bf39fb0
Haekal Rizqi Ramadhan <rizqihaekal014@gmail.com>
2025-10-17 22:59:51 +0800

    Pertemuan 6

---
+++ 01_Struktur_Dasar/test3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman PHP Pertamaku</title>
+</head>
+<body>
+    <h1>Script PHP dalam (x)HTML</h1>
+    <?php
+    echo "Hello World!";
+    echo "Hello World!";
+    echo "Hello World!";
+    echo "Hello World!";
+    echo "Hello World!";
+    ?>
+</body>
+</html>
