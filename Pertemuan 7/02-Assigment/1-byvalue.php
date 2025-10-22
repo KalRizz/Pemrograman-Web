ebb108e3f4e7b54fe1b8587a7e4ecac4958bf60c
KalRizz <rizqihaekal014@gmail.com>
2025-10-22 14:56:06 +0800

    Pertemuan 7

---
+++ Pertemuan 7/02-Assigment/1-byvalue.php
@@ -0,0 +1,13 @@
+<?php
+$a = 20;
+$b = 15;
+$c = 5;
+
+echo "\$a = $a, \$b = $b, \$c = $c";
+echo "<br />";
+// hasil proses: $a = 20 $b = 15, $c = 5
+
+$a = $b = $c;
+echo "\$a = $a, \$b = $b, \$c = $c";
+// hasil proses: $a = 5 $b = 5, $c = 5
+?>
